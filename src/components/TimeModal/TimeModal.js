f5ecba220a56e0c1ea168d316641a95fbf57b7f4
Haider Ali <malikhaider0567@gmail.com>
2022-10-30 15:42:22 +0530

    State Management Using Redux Remaining

--- src/components/TimeModal/TimeModal.js
+++ src/components/TimeModal/TimeModal.js
@@ -3,17 +3,38 @@ import './timemodal.css';
 
 const TimeModal = () => {
 	const [info, setInfo] = useState({ name: '', phone: '' });
+
+	const infoChange = (e) => {
+		setInfo({ ...info, [e.target.name]: e.target.value });
+	};
+
 	return (
-		<div className="modal_wrapper">
+		<div className="modal_wrapper active">
 			<div className="modal_body">
 				<div className="modal_header">
 					<h2>Timings</h2>
-					<button className="modal_close_btn">Close</button>
+					<button className="modal_close_btn">×</button>
 				</div>
-				<form>
-					<input type="text" name="name" id="name" />
-					<input type="number" name="phone" id="phone" />
-					<button type="submit">Update</button>
+				<form className="modal_form">
+					<input
+						placeholder="Name"
+						type="text"
+						name="name"
+						id="name"
+						value={info.name}
+						onChange={infoChange}
+					/>
+					<input
+						placeholder="Phone"
+						type="number"
+						name="phone"
+						id="phone"
+						value={info.phone}
+						onChange={infoChange}
+					/>
+					<button type="submit" className="updateBtn">
+						Update
+					</button>
 				</form>
 			</div>
 		</div>
